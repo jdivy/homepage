10373bd29ef19577df0cff216180f003668aa7d3
Jack Ivy <jack@jackivy.com>
2013-10-26 20:26:33 -0400

    First pass at homepage.

---
+++ js/jack.js
@@ -0,0 +1,18 @@
+var $root = $('html, body');
+$('a.slide').click(function() {
+    var href = $.attr(this, 'href');
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        //window.location.hash = href;
+    });
+    return false;
+});
+
+$('#memtip').tooltip({
+   title : "Say what?! Memphis is Jack's hometown, but more importantly it is the 'SuperHub' of FedEx. Two words: Tennessee Brewery."
+});
+
+$('#worktip').tooltip({
+   title : "By no means is this a reflection of Jack's willingness to work for anyone else!"
+});
